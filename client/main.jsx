510bd341badbe206e7193aec0b3ff9a001d86fc0
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-05-03 13:23:30 +0300

    setup schema for mongo

--- client/main.jsx
+++ client/main.jsx
@@ -2,11 +2,22 @@ import React from 'react'
 import { Meteor } from 'meteor/meteor'
 import { render } from 'react-dom'
 import injectTapEventPlugin from 'react-tap-event-plugin'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import App from '../imports/ui/App'
+import Example from '../imports/ui/Examples'
+import Lost from '../imports/ui/Lost'
 
 injectTapEventPlugin()
 
 Meteor.startup(() => {
-  render(<App />, document.getElementById('render-target'))
+  render((
+    <Router>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/example" component={Example} />
+        <Route component={Lost} />
+      </Switch>
+    </Router>
+  ), document.getElementById('render-target'))
 })
